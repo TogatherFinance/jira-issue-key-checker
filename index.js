8372fedb25a5be37a39ce8d384de6fc22a06b059
Alper <aradhell@users.noreply.github.com>
2020-11-13 20:01:47 +0100

    update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ async function run() {
     const prBody = github.context.payload.pull_request.body;
 
     let regex = new RegExp(`${jiraPrefix}-[0-9]+`);
-    let regexBody = new RegExp(`https:\/\/togather\.atlassian\.net+`);
+    let regexBody = new RegExp(`togather\.atlassian\.net+`);
     if (!regex.test(prTitle) || !regexBody.test(prBody)) {
       core.setFailed("Jira Issue Key missing in PR title or description. " + prTitle + " prefix : " + jiraPrefix);
       return;
